aff9a748a8ebafac1bac157136a0eef4389ce5f4
Ariel Feingold <arielfeingold@Andriels-MacBook-Pro.local>
2018-04-06 15:24:00 -0400

    added slug to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ActiveRecord::Base
   has_many :items
   has_many :outfits
 
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
 
 end
