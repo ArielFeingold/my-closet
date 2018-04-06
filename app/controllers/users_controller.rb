0e3107c9bb05ab30090b9f27428ec526b2a89472
Ariel Feingold <arielfeingold@Andriels-MacBook-Pro.local>
2018-04-06 16:15:30 -0400

    added signup controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
- 
+
   get '/signup' do
-    erb: 'users/signup'
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      erb :'users/show'
+    end
   end
 
-end
+
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/show'
+    end
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect to "/users/#{@user.slug}"
+  end
+
+  # get '/logout' do
+  #   if logged_in?
+  #     session.destroy
+  #     redirect to '/login'
+  #   else
+  #     redirect to '/'
+  #   end
+  # end
