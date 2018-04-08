a1ce762ca6a211767f0206a6aaa6a6dfe9cad4cd
Ariel Feingold <arielfeingold@Andriels-MBP.fios-router.home>
2018-04-08 13:06:50 -0400

    added outfit show page

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -6,23 +6,24 @@ class OutfitsController < ApplicationController
     erb :'outfits/outfits'
   end
 
+  post '/outfits' do
+    @outfit = Outfit.new(params[:outfit])
+    @outfit.user_id = session[:user_id]
+    @outfit.save
+    redirect to '/outfits'
+  end
+
+  get '/outfits/:id' do
+    @outfit = Outfit.find_by(params[:id])
+    # binding.pry
+    erb :'outfits/show'
+  end
+
   get '/outfits/new' do
     @user = User.find_by(:id => session[:user_id])
     erb :'outfits/new'
   end
 
-  post '/outfits' do
-    @outfit = Outfit.new(params[:outfit])
-    @outfit.user_id = session[:user_id]
-    @outfit.save
-    binding.pry
-    # params[:outfit][:item_ids].each do |item_id|
-    #   binding.pry
-    #   item_id = item_id.to_i
-    #   item = Item.find_by(:id => params[:outfit][:item_ids][item_id])
-    #   @outfit.items << item
-    # end
-    redirect to '/outfits'
-  end
+
 
 end
