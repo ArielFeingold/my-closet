42fdd8bd865bba31c153bc2c19e96ac0a68197ac
ArielFeingold <feingold.ariel@gmail.com>
2018-04-09 11:35:48 -0400

    added outfit show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,20 @@
 class ItemsController < ApplicationController
 
+    get '/items' do
+    if logged_in?
+      @categories = []
+      @items = Item.all.find_all{|i| i.user_id == session[:user_id]}
+      @items.each do |item|
+        if !@categories.include?(item.category)
+          @categories << item.category
+        end
+      end
+      erb :'items/items'
+    else
+      redirect to '/login'
+    end
+  end
+
   get '/items/new' do
     if logged_in?
       erb :'/items/create_item'
@@ -19,33 +34,30 @@ class ItemsController < ApplicationController
     end
   end
 
-  get '/items/:id/edit' do
-    @item = Item.find_by(params)
-    erb :'items/edit_item'
-  end
-
   get '/items/:id' do
     if logged_in?
       @item = Item.find_by(params)
-      erb :'items/show'
     else
       redirect to '/login'
     end
   end
 
+  get '/items/:id/edit' do
+    @item = Item.find_by(params)
+    erb :'items/edit_item'
+  end
+
   patch '/items/:id' do
     if logged_in?
       @item = Item.find_by(:id=> params[:id])
-        if @item && @item.user == current_user
-          @item.update(name: params[:name], category: params[:category], item_type: params[:item_type], color: params[:color])
-          redirect to "/items/#{@item.id}"
-        else
-          redirect to "/items/#{@item.id}/edit"
-        end
+      if @item && @item.user == current_user
+        @item.update(name: params[:name], category: params[:category], item_type: params[:item_type], color: params[:color])
+        redirect to "/items/#{@item.id}"
       else
-        redirect to '/login'
+        redirect to "/items/#{@item.id}/edit"
       end
+    redirect to '/login'
     end
-
+  end
 
 end
